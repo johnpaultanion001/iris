610ca2f3630e383184d500b35d391905f0f99aff
Alyssie Öö <caluyaalyssamae@gmail.com>
2023-05-01 16:15:55 +0800

    Layout
    Navbar, Footer, Sidebar

---
+++ resources/js/components/utilities/navigation.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="navigation grid gap-1 grid-cols-5 xl:grid-cols-6 mb-4">
+        <div class="col-span-1 flex items-center">
+            <router-link to="/"><img src="/img/icon/home-white.png"> </router-link>
+        </div>
+        <div class="col-span-1 flex items-center">
+            <button @click="setMenu"><img src="/img/icon/menu-white.png"> </button>
+        </div>
+        <div class="col-span-3 xl:col-span-4 flex items-center">
+            <div class="w-full flex items-center justify-end">
+                <img :src="userImg" class="mr-2 w-9 h-9 rounded-full"> 
+                <p class="text-base text-white font-inter-700">Hi, {{ userFirstName }} 👋</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup: () => ({
+        title: 'Navidation'
+    }),
+    data() {
+        return {
+            userFirstName: "Jesse",
+            userImg: "/img/user-photo.png",
+        };
+    },
+    emits: ["menu"],
+    methods: {
+        setMenu() {
+            this.$emit("menu", true);
+        },
+    },
+}
+</script>
--- resources/js/components/utilities/sidebar.vue
+++ resources/js/components/utilities/sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="p-4 h-screen">
+    <div class="py-4 pl-2 h-screen">
         <div class="sidebar bg-lighter rounded-2xl h-full w-full overflow-hidden shadow-secondary">
             <div class="sidebar-header p-4 border border-t-0 border-x-0 border-b-1-light">
                 <img src="/img/iris-logo-blue.png" class="mx-auto"> 
@@ -12,44 +12,60 @@
                     <p class="text-sm text-blue-grey font-opensans-600">{{ agencyName }}</p>
                 </div>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Dashboard' ? 'bg-lighter' : 'bg-white'" to="/">
-                    <img v-if="pageName === 'Dashboard'" src="/img/icon/home-active.png" class="mr-3.5">
-                    <img v-else src="/img/icon/home.png" class="mr-3.5"> 
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Dashboard'" src="/img/icon/home-active.png" class="absolute">
+                        <img src="/img/icon/home.png"> 
+                    </div>
                     <p :class="pageName === 'Dashboard' ? 'text-blue' : 'text-blue-grey'">Dashboard</p>
                 </router-link>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Inbox' ? 'bg-lighter' : 'bg-white'" to="/inbox">
-                    <img v-if="pageName === 'Inbox'" src="/img/icon/inbox-active.png" class="mr-3.5"> 
-                    <img v-else src="/img/icon/inbox.png" class="mr-3.5"> 
-                    Inbox
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Inbox'" src="/img/icon/inbox-active.png" class="absolute"> 
+                        <img src="/img/icon/inbox.png" class="mr-3.5"> 
+                    </div>
+                    <p :class="pageName === 'Inbox' ? 'text-blue' : 'text-blue-grey'">Inbox</p>
                 </router-link>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Tickets' ? 'bg-lighter' : 'bg-white'" to="/tickets">
-                    <img v-if="pageName === 'Tickets'" src="/img/icon/tickets-active.png" class="mr-3.5"> 
-                    <img v-else src="/img/icon/tickets.png" class="mr-3.5"> 
-                    Tickets
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Tickets'" src="/img/icon/tickets-active.png" class="absolute"> 
+                        <img src="/img/icon/tickets.png" class="mr-3.5"> 
+                    </div>
+                    <p :class="pageName === 'Tickets' ? 'text-blue' : 'text-blue-grey'">Tickets</p>
                 </router-link>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Reported Vendors' ? 'bg-lighter' : 'bg-white'" to="/reported-vendors">
-                    <img v-if="pageName === 'Reported Vendors'" src="/img/icon/reported-vendors-active.png" class="mr-3.5"> 
-                    <img v-else src="/img/icon/reported-vendors.png" class="mr-3.5"> 
-                    Reported Vendors
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Reported Vendors'" src="/img/icon/reported-vendors-active.png" class="absolute"> 
+                        <img src="/img/icon/reported-vendors.png" class="mr-3.5"> 
+                    </div>
+                    <p :class="pageName === 'Reported Vendors' ? 'text-blue' : 'text-blue-grey'">Reported Vendors</p>
                 </router-link>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Users' ? 'bg-lighter' : 'bg-white'" to="/users">
-                    <img v-if="pageName === 'Users'" src="/img/icon/users-active.png" class="mr-3.5"> 
-                    <img v-else src="/img/icon/users.png" class="mr-3.5"> 
-                    Users
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Users'" src="/img/icon/users-active.png" class="absolute"> 
+                        <img src="/img/icon/users.png" class="mr-3.5"> 
+                    </div>
+                    <p :class="pageName === 'Users' ? 'text-blue' : 'text-blue-grey'">Users</p>
                 </router-link>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Reports' ? 'bg-lighter' : 'bg-white'" to="/reports">
-                    <img v-if="pageName === 'Reports'" src="/img/icon/reports-active.png" class="mr-3.5"> 
-                    <img v-else src="/img/icon/reports.png" class="mr-3.5"> 
-                    Reports
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Reports'" src="/img/icon/reports-active.png" class="absolute"> 
+                        <img src="/img/icon/reports.png" class="mr-3.5"> 
+                    </div>
+                    <p :class="pageName === 'Reports' ? 'text-blue' : 'text-blue-grey'">Reports</p>
                 </router-link>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Settings' ? 'bg-lighter' : 'bg-white'" to="/settings">
-                    <img v-if="pageName === 'Settings'" src="/img/icon/settings-active.png" class="mr-3.5"> 
-                    <img v-else src="/img/icon/settings.png" class="mr-3.5"> 
-                    Settings
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Settings'" src="/img/icon/settings-active.png" class="absolute"> 
+                        <img src="/img/icon/settings.png" class="mr-3.5"> 
+                    </div>
+                    <p :class="pageName === 'Settings' ? 'text-blue' : 'text-blue-grey'">Settings</p>
                 </router-link>
                 <router-link class="w-full flex items-center p-5 text-sm font-opensans-600 hover:bg-lighter" :class="pageName === 'Activity' ? 'bg-lighter' : 'bg-white'" to="/activity">
-                    <img v-if="pageName === 'Activity'" src="/img/icon/activity-active.png" class="mr-3.5"> 
-                    <img v-else src="/img/icon/activity.png" class="mr-3.5"> 
-                    Activity
+                    <div class="mr-3.5 relative">
+                        <img v-if="pageName === 'Activity'" src="/img/icon/activity-active.png" class="absolute"> 
+                        <img src="/img/icon/activity.png" class="mr-3.5"> 
+                    </div>
+                    <p :class="pageName === 'Activity' ? 'text-blue' : 'text-blue-grey'">Activity</p>
                 </router-link>
             </div>
         </div>
@@ -57,11 +73,12 @@
 </template>
 
 <script>
+
 export default {
     setup: () => ({
         title: 'Sidebar'
     }),
-    props: ['pageName'],
+    props: ['pageName', 'show'],
     data() {
         return {
             agencyName: "Department of Trade & Industry",
