1728657949891b2fad04507ea036ff99b580e6eb
Alyssie Öö <caluyaalyssamae@gmail.com>
2023-05-10 09:27:08 +0800

    Ticket Page
    Ticket Page

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://dti-internal.ap-southeast-1.elasticbeanstalk.com/';
+axios.defaults.baseURL = 'https://iris.supsofttech.com/';
--- resources/js/components/utilities/contentCard.vue
+++ resources/js/components/utilities/contentCard.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="content-card bg-white rounded-2xl p-4 shadow-main">
-        <h6 class="font-opensans-600 text-base text-dark mb-2.5">{{ cardTitle }}</h6>
+    <div class="content-card bg-white rounded-2xl p-4 shadow-main relative">
+        <h6 v-if="cardTitle" class="font-opensans-600 text-base text-dark mb-2.5">{{ cardTitle }}</h6>
         <slot></slot>
     </div>
 </template>
