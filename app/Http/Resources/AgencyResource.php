b4f477a2978e9ddbd78930c4eab5a1efc6b9e3da
Johnpaul Tanion <johnpaultanion001@gmail.com>
2023-05-31 11:14:51 +0800

    Agency Resource

--- app/Http/Resources/AgencyResource.php
+++ app/Http/Resources/AgencyResource.php
@@ -16,10 +16,6 @@ class AgencyResource extends ApiResource
       return [
 
 
-          'logo' => $this->agency->logo ?? $this->logo ?? '0',
-          'agency' => $this->agency->title ?? $this->title ?? '0',
-          'code' => $this->agency->code ?? $this->code ?? '0',
-          'color' => $this->agency->color ?? $this->color ?? '0',
 
       ];
 
